f9201998b9b55f5f57ac4d644acb2db46bd0dce1
John Gill <swfiua@gmail.com>
2016-01-16 18:22:03 -0400

    very minimal version of md2json

--- md2json.py
+++ md2json.py
@@ -5,3 +5,36 @@ For now, I am trying to read a talk outline and turn it into something
 that will create a slideshow.
 
 """
+
+import os
+import sys
+
+import getopt
+
+
+class Mark2Json:
+
+    def __init__(self):
+
+        pass
+
+    def interpret(self, infile):
+        """ Process a file of rest and return json """
+
+        data = []
+        for line infile:
+            record = {}
+            if line.startswith('#')
+                record['caption'] = line[1:].strip()
+
+            data.append(record)
+
+        return json.dumps(data)
+
+
+if __name__ == '__main__':
+
+    mark2json = Mark2Json()
+
+    return mark2json.interpret(sys.stdin.read())
+
