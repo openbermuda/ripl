1ee21e53bc6d1be60354aa85e9c384d973075fee
John Gill <swfiua@gmail.com>
2016-01-17 20:24:44 -0400

    add images get more than i bargained for

--- md2json.py
+++ md2json.py
@@ -12,7 +12,6 @@ import json
 
 import getopt
 
-
 class Mark2Json:
 
     def __init__(self):
@@ -23,15 +22,55 @@ class Mark2Json:
         """ Process a file of rest and return json """
 
         data = []
-        for line in infile:
-            record = {}
-            if line.startswith('#'):
-                record['caption'] = line[1:].strip()
 
-            if record:
-                data.append(record)
+        for record in self.generate_records(infile):
+            data.append(record)
 
         return json.dumps(data)
+
+            
+    def generate_records(self, infile):
+        """ Process a file of rest and yield dictionaries """
+        for line in infile:
+            record = {}
+
+            # any Markdown heading is just a caption, no image
+            print(line)
+            if line.startswith('#'):
+                print(line)
+                record['caption'] = line[1:].strip()
+                yield record
+                continue
+
+            fields = line.split(',')
+
+            # nothing there, carry on
+            if not fields: continue
+
+            image = fields[0].strip()
+
+            if not image: continue
+
+            record['image'] = image
+
+            try:
+                time = float(fields[1])
+            except:
+                time = 0
+
+            record['time'] = time
+
+            try:
+                caption = fields[2].strip()
+            except:
+                caption = None
+
+            if caption:
+                record['caption'] = caption
+
+            # yield it if we have anything
+            if record:
+                yield record
 
 
 if __name__ == '__main__':
